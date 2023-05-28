32b2f5892b91b167de33716fe1c5181dc8daa953
lucashadin <62254402+lucashadin@users.noreply.github.com>
2023-05-28 20:54:46 +0200

    demos

---
+++ Demos/index.tsx
@@ -0,0 +1,89 @@
+import { HDKComponent, HNode, Prefab, render, useRandom } from '@hiber3d/hdk-react';
+import {
+  AsteroidSpinning,
+  Avatar,
+  Distribute,
+  Ground,
+  Orbiting,
+  RandomTilt,
+  Room,
+  Spawnpoint,
+  VideoPanel,
+} from '@hiber3d/hdk-react-components';
+ 
+const Foliage: HDKComponent = props => {
+  return (
+    <Distribute
+      {...props}
+      gladeRadius={20}
+      renderItem={item => {
+        const random = useRandom();
+ 
+        return (
+          <RandomTilt scale={random.range(1, 5)}>
+            <Prefab id={random.fromArray(['candy_straps', 'trashcontainer_01_t1', 'mushroom_01_t3'])} />
+          </RandomTilt>
+        );
+      }}
+    />
+  );
+};
+ 
+const OrbitingAsteroids: HDKComponent = props => {
+  return (
+    <HNode {...props}>
+      <Orbiting duration={64}>
+        <Distribute
+          gladeRadius={40}
+          outerBoundRadius={100}
+          itemAreaSizeMin={15}
+          itemAreaSizeMax={25}
+          renderItem={item => {
+            const random = useRandom();
+ 
+            return (
+              <AsteroidSpinning y={random.range(-10, 10)} scale={random.range(1, 5)}>
+                <Prefab id={random.fromArray(['house_01'])} />
+              </AsteroidSpinning>
+            );
+          }}
+        />
+      </Orbiting>
+    </HNode>
+  );
+};
+ 
+const Building: HDKComponent = props => {
+  return (
+    <Room
+      {...props}
+      floorThickness={1}
+      dim={7}
+      roof={false}
+      wallThickness={0.5}
+      height={10}
+      windowWidth={3}
+      // wallMaterial={'t_neon_grid_01'}
+      wallTypes={['WINDOW', 'WINDOW', 'DOOR', 'WINDOW']}>
+      <Prefab id="sofa_01_t2" rotY={180} scale={2}>
+        <Avatar animation="an_default_backflip" y={0.16} z={0.12} rotY={20} />
+      </Prefab>
+      <Prefab id="floor_lamp_01" rotY={210} x={4} y={0.1} scale={2} />
+      <VideoPanel src={'https://cdn.hibervr.com/video/Hiber3D.mp4'} y={7} z={-8.5} scale={5} rotY={180}></VideoPanel>
+    </Room>
+  );
+};
+ 
+const World = () => (
+  <HNode>
+    <Foliage y={-1.5} />
+    {/* <OrbitingAsteroids y={20} /> */}
+    <Building y={0} z={20} />
+    <Spawnpoint />
+    <Ground hilly={0} material="t_gore_01" />
+    <Ground hilly={0} material="t_swamp_ground" water={true} y={-1}/>
+    <Ground hilly={30} y={45} rotZ={180} material="t_cobble_stone_01" />
+  </HNode>
+);
+ 
+render(<World />, { environment: 'sunrise_01' });
