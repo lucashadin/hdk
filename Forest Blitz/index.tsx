b4f1049c54257a4bdd080259702ae66c639dde9c
lucashadin <62254402+lucashadin@users.noreply.github.com>
2023-05-30 15:12:19 +0200

    forest blitz

--- Forest Blitz/index.tsx
+++ Forest Blitz/index.tsx
@@ -1,5 +1,5 @@
 import { HDKComponent, HNode, Prefab, render, Animation, InfoPanel, useRandom } from '@hiber3d/hdk-react';
-import { AsteroidSpinning, Distribute, Ground, Hovering, InCircle, Path, RandomPosition,RandomTilt, Spawnpoint } from '@hiber3d/hdk-react-components';
+import { AsteroidSpinning, Distribute, Ground, Hovering, InCircle, Path, RandomPosition, RandomTilt, Spawnpoint, Damaging } from '@hiber3d/hdk-react-components';
 
 
 
@@ -107,151 +107,196 @@ const TippingRocks: HDKComponent<TippingRocksProps> = ({ rx = 0, ...props }) => 
       easing: 'EASE_IN_CUBIC',
     }}
   >
-    <Prefab id="cliff_02_02" />
+    <Prefab id="dead_tree_02" />
+    <Prefab id="tree_02" scale={1}/>
   </Animation>
 );
 
 const GroundGroup = () => (
   <HNode>
     {/* <Ground hilly={0.5} y=s{-0.5} material="t_sand_01" water="True" repeatX={1} repeatZ={1} /> */}
-    <Ground hilly={0} material="t_sand_01" scaleX={0.2} scaleZ={0.2} repeatX={1} repeatZ={1} />
+    <Ground hilly={0.0} material="t_bark_01" scaleX={0.2} scaleZ={0.1} repeatX={1} repeatZ={1} />
     {/* <Ground hilly={10} y={40} rotZ={180} scaleX={5} scaleZ={5} material="t_bark_02" repeatX={1} repeatZ={1} /> */}
   </HNode>
 )
 
+const Sides: HDKComponent = props => {
+  const random = useRandom()
+  return  (
+  <Prefab
+    {...props}
+    id="tree_02"
+    // material="t_rock_02"
+
+  />
+  
+  
+  )
+    }
+
+
+
 const ValleyWalls = () => {
   const random = useRandom();
   return (
-  <Path
-    showPoints={false}
-    numberOfItems={100}
-    tension={0.8}
-    points={[
-      [45, 0, 30], [45, 0, 99], [75, 0, 171], [126, 0, 207], [165, 0, 240], [216, 0, 273], [297, 0, 270], [363, 0, 240], [390, 0, 168], [348, 0, 96], [288, 0, 87], [246, 0, 111], [192, 0, 159], [159, 0, 207], [117, 0, 240], [96, 0, 288], [87, 0, 345], [90, 0, 414], [84, 0, 477], [105, 0, 519], [144, 0, 537], [201, 0, 525], [255, 0, 507], [294, 0, 504], [342, 0, 483], [393, 0, 483], [441, 0, 507], [441, 0, 573], [408, 0, 624], [339, 0, 657], [252, 0, 633], [249, 0, 591], [249, 0, 546], [243, 0, 456], [255, 0, 408], [306, 0, 378], [366, 0, 372], [450, 0, 369]
+    <Path
+      showPoints={true}
+      numberOfItems={400}
+      tension={0.8}
+      points={[
+        [78, 0, 60], [132, 0, 132], [192, 0, 201], [282, 10, 225], [378, 20, 177], [378, 30, 72], [294, 40, 51], [216, 40, 78], [129, 40, 129], [69, 0, 189], [60, 0, 288], [69, 0, 381], [120, -20, 459], [195, -30, 507], [291, -30, 519], [375, -20, 492], [447, -10, 426], [468, 0, 303], [354, 5, 276], [285, 10, 309], [252, 15, 384], [291, 20, 450], [366, 25, 486], [465, 30, 573], [540, 35, 630], [636, 5, 624], [714, 0, 579], [735, 0, 489], [726, 0, 372], [690, 0, 267], [624, 0, 180]
 
 
 
-      // [400, 0, 300],
-    ]}
-    // close={true}
+      
+      
 
-    // x={35.9} y={12.1} z={110.6}
+        // [400, 0, 300],
+      ]}
+      // close={true}
 
-    renderItem={step => {
-      console.log(step.rotation[1])
-      if(step.position[0] >125 && step.position[0] < 140){
-        return null
-      }
-      return (
-        <HNode >
+      // x={35.9} y={12.1} z={110.6}
 
-          <Prefab
-            id="cliff_01_wall"
-            material="t_rock_02"
-            scaleY={3}
-            scaleZ={3}
-            rotY={90}
-            x={-15}
-          // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
-          // rotY={step.rotation[1]}
-          />
-          <Prefab
-            id="cliff_01_wall"
-            material="t_rock_02"
-            scaleY={3}
-            scaleZ={3}
-            rotY={90}
-            x={15}
-          // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
-          // rotY={step.rotation[1]}
-          />
+      renderItem={step => {
+        console.log(step.rotation[1])
+        if ((step.index % 20 == 0 || step.index % 20 == 1)  && step.index > 10) {
+          return null
+        }
 
-          <GroundGroup />
-          {/* <PlantsDecoration /> */}
-          {/* <RocksDecoration /> */}
+        // if (step.position[0] > 375 && step.position[0] < 385) {
+        //   return null
+        // }
+        return (
+          <HNode >
+            
+            
+            <Sides  rotY={90} x={-15} scaleX={3} scaleY={random.range(2, 4)}/>
+            
+
+            
+            <Sides  rotY={90} x={15} scaleX={3} scaleY={random.range(2, 4)}/>
+            
+           
+
+{/* 
+            <Prefab
+              id="tree_02"
+              // material="t_grass_01"
+              scaleY={3}
+              scaleZ={3}
+              rotY={90}
+              x={-15}
+            // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
+            // rotY={step.rotation[1]}
+            />
+            <Prefab
+              id="tree_02"
+              // material="t_rock_02"
+              scaleY={3}
+              scaleZ={3}
+              rotY={90}
+              x={15}
+            // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
+            // rotY={step.rotation[1]}
+            /> */}
+
+            <GroundGroup />
+            {/* <PlantsDecoration /> */}
+            {/* <RocksDecoration /> */}
 
 
 
-          {step.index % 3 === 0 && (
-            <>
-              <Prefab
-                id="gpl_booster_plate_02"
-                // material="palette_01_green"
-                x={random.range(-5, 5)}
-                y={0.2}
-                rotY={-180}
-                rotX={20}
-                scale={1.5}
+            {step.index % 3 === 0  && (
+              <>
+                <Prefab
+                  id="gpl_booster_plate_02"
+                  // material="t_grid_01"
+                  x={random.range(-5, 5)}
+                  y={0.2}
+                  rotY={-180}
+                  rotX={20}
+                  scale={2}
+                />
+              </>
+
+            )}
+
+            {/* {step.index % 6 === 1 && (
+              <>
+                <Damaging amount={50} knockbackStrength={200} >
+                <Prefab
+                  id="waffle_01"
+                  material="t_lava_01"
+                  x={random.range(-10, 10)}
+                  y={0}
+                  rotY={random.range(0, 360)}
+                  scale={1}
+                />
+                </Damaging>
+              </>
+            )} */}
+
+
+            {step.index % 10 === 1 && step.index > 20 && (
+
+              <TippingRocks x={-10} rotY={90} scaleY={1} />
+
+            )}
+
+            {step.index % 100 === 1 && step.index > 20 && (
+
+              <Prefab 
+              id="collectible_mandatory_key_01" 
+              material='t_rainbow_02'
+              y={-5}
+              rotY={90} 
+              scale={7} 
               />
-            </>
 
-          )}
-
-          {step.index % 3 === 1 && (
-            <>
-              <Prefab
-                id="h_rolling_candy_ball_01"
-                material="t_bark_01"
-                x={-10}
-                y={0}
-                rotY={0}
-                scale={1}
-              />
-            </>
+            )}
 
 
+                        
+            {step.index % 50 === 1 && step.index > 20  && (
 
-          )}
-          {/* {step.index % 5 === 1 && (
-            <>
-              <Prefab
-                id="checkpoint_01"
-                // material="t_lava_01"
-                x={random.range(-5, 5)}
-                y={0.0}
-                rotY={0}
-                rotZ={0}
-                scale={1.5}
-              />
-            </>
+            <Prefab 
+            id="checkpoint_01" 
+            // material='t_rainbow_02'
+            y={0}
+            rotY={90} 
+            scale={5} 
+            />
 
+            )}
 
-          )} */}
+            {step.index % 15 === 1 && (
 
-          {step.index % 10 === 1 && (
+              <TippingRocks x={10} rotY={-90} scaleY={1} />
 
-            <TippingRocks x={-10} rotY={90} scaleY={1} />
+            )}
 
-          )}
-
-          {step.index % 15 === 1 && (
-
-            <TippingRocks x={10} rotY={-90} scaleY={1} />
-
-          )}
-
-        </HNode>
-      )
-    }}></Path>
+          </HNode>
+        )
+      }}></Path>
   )
-  }
+}
 
 const Goal = () => (
   <>
-  <Prefab
-    id="goal_01"
-    material="t_planks_02"
-    scaleY={3}
-    x={286.6} y={0.0} z={128.4}
-  />
-  <Prefab
-    id="rock_cube_01_t2"
-    material="t_planks_02"
-    scaleY={1}
-    x={291.4} y={-2.0} z={125.8}
-    scaleX={5}
-    scaleZ={5}
-  />
+    <Prefab
+      id="goal_01"
+      material="t_planks_02"
+      scaleY={3}
+      x={624.9} y={0.0} z={179.5}
+    />
+    <Prefab
+      id="rock_cube_01_t2"
+      material="t_planks_02"
+      scaleY={1}
+      x={291.4} y={-2.0} z={125.8}
+      scaleX={5}
+      scaleZ={5}
+    />
   </>
 )
 
@@ -273,16 +318,16 @@ const MossyMound = () => (
     showPoints={false}
     numberOfItems={20}
     points={[
-      [297, 0, 135],
-      [297, 0, 119],
-      [287, 0, 119]
+      [623, 0, 178],
+      [623, 0, 178]
+      
       // [400, 0, 300],
     ]}
     // close={true}
     renderItem={step => {
       // console.log(step.rotation[1])
       return (
-        <HNode rotY={step.rotation[1]}>
+        <HNode>
 
 
           {step.index % 2 === 0 && (
@@ -313,30 +358,14 @@ const MossyMound = () => (
     }}></Path>
 )
 
-const PythonPath = () => (
-<Path
-  points={[
-    [0, 0, 0],
-    [30, 0, 0],
-    [20, 20, 40],
-    [30, 0, 80],
-  ]}
-  numberOfItems={50}
-  renderItem={() => (
-    <>
-      <Prefab id="cube_01" />
-      {/* <Prefab id="gpl_booster_plate_01" y={3} rotY={-180} /> */}
-    </>
-  )}
-/>
-)
+
 
 
 const WelcomeSign: HDKComponent = props => (
   <InfoPanel
 
-    header="Welcome to The Canyon!"
-    body="Jump or slide to keep your speed. Watch out for falling rocks!"
+    header="Welcome to The Forest!"
+    body="Jump or slide to keep your speed. Watch out for falling trees!"
     // url="https://developer.hiber3d.com/docs"
     openUrlInNewTab={true}>
     <Prefab
@@ -347,18 +376,18 @@ const WelcomeSign: HDKComponent = props => (
 
 const World = () => (
   <HNode>
-    <Spawnpoint x={39.2} y={4.8} z={25.8} rotY={-90} />
-    <ValleyWalls/>
+    <Spawnpoint x={72.2} y={0.0} z={56.5} rotY={-120} />
+    <ValleyWalls />
     <Goal />
     {/* <Treehouse /> */}
     {/* <MossyMound /> */}
-    {/* <WelcomeSign x={-0.5} y={0.4} z={6} rotY={-90} /> */}
+    <WelcomeSign x={74.4} y={0.0} z={57.5} rotY={-90} />
 
 
   </HNode >
 );
 
-render(<World />, { environment: 'midday_clear_01' });
+render(<World />, { environment: 'hiberpunk_smog_01' });
 
 // questions
 // two prefabs in one step
