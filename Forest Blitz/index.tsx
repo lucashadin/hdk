6b2f4726e6217c6a3d95805dd5c74071bab79478
lucashadin <62254402+lucashadin@users.noreply.github.com>
2023-05-30 16:35:54 +0200

    optimizing

--- Forest Blitz/index.tsx
+++ Forest Blitz/index.tsx
@@ -108,7 +108,7 @@ const TippingRocks: HDKComponent<TippingRocksProps> = ({ rx = 0, ...props }) => 
     }}
   >
     <Prefab id="dead_tree_02" />
-    <Prefab id="tree_02" scale={1}/>
+    <Prefab id="tree_02" scale={1} />
   </Animation>
 );
 
@@ -122,17 +122,17 @@ const GroundGroup = () => (
 
 const Sides: HDKComponent = props => {
   const random = useRandom()
-  return  (
-  <Prefab
-    {...props}
-    id="tree_02"
+  return (
+    <Prefab
+      {...props}
+      id="tree_02"
     // material="t_rock_02"
 
-  />
-  
-  
+    />
+
+
   )
-    }
+}
 
 
 
@@ -140,16 +140,16 @@ const ValleyWalls = () => {
   const random = useRandom();
   return (
     <Path
-      showPoints={true}
-      numberOfItems={400}
+      showPoints={false}
+      numberOfItems={250}
       tension={0.8}
       points={[
-        [78, 0, 60], [132, 0, 132], [192, 0, 201], [282, 10, 225], [378, 20, 177], [378, 30, 72], [294, 40, 51], [216, 40, 78], [129, 40, 129], [69, 0, 189], [60, 0, 288], [69, 0, 381], [120, -20, 459], [195, -30, 507], [291, -30, 519], [375, -20, 492], [447, -10, 426], [468, 0, 303], [354, 5, 276], [285, 10, 309], [252, 15, 384], [291, 20, 450], [366, 25, 486], [465, 30, 573], [540, 35, 630], [636, 5, 624], [714, 0, 579], [735, 0, 489], [726, 0, 372], [690, 0, 267], [624, 0, 180]
+        [78, 0, 60], [132, 0, 132], [192, 0, 201], [282, 10, 225], [378, 20, 177], [378, 30, 72], [294, 40, 51], [216, 40, 78], [129, 40, 129], [69, 20, 189], [60, 0, 288], [69, 0, 381], [120, -20, 459], [195, -30, 507], [291, -30, 519], [375, -20, 492], [447, -10, 426], [468, 0, 303], [354, 5, 276], [285, 10, 309], [252, 15, 384], [291, 20, 450], [366, 25, 486], [465, 30, 573], [540, 35, 630], [636, 20, 624], [714, 5, 579], [735, 0, 489], [726, 0, 372], [690, 0, 267], [624, 0, 180]
 
 
 
-      
-      
+
+
 
         // [400, 0, 300],
       ]}
@@ -158,47 +158,41 @@ const ValleyWalls = () => {
       // x={35.9} y={12.1} z={110.6}
 
       renderItem={step => {
-        console.log(step.rotation[1])
-        if ((step.index % 20 == 0 || step.index % 20 == 1)  && step.index > 10) {
-          return null
+        // console.log(step.rotation[1])
+        if ((step.index % 10 == 0) && step.index > 10) {
+
+          return <Prefab
+            id="campfire_01"
+            // material="t_grid_01"
+            x={0}
+            y={-5}
+            rotY={-180}
+            // rotX={20}
+            scaleX={20}
+            scaleZ={20}
+            scaleY={1}
+          />
         }
+
+
+
 
         // if (step.position[0] > 375 && step.position[0] < 385) {
         //   return null
         // }
         return (
           <HNode >
-            
-            
-            <Sides  rotY={90} x={-15} scaleX={3} scaleY={random.range(2, 4)}/>
-            
 
-            
-            <Sides  rotY={90} x={15} scaleX={3} scaleY={random.range(2, 4)}/>
-            
-           
 
-{/* 
-            <Prefab
-              id="tree_02"
-              // material="t_grass_01"
-              scaleY={3}
-              scaleZ={3}
-              rotY={90}
-              x={-15}
-            // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
-            // rotY={step.rotation[1]}
-            />
-            <Prefab
-              id="tree_02"
-              // material="t_rock_02"
-              scaleY={3}
-              scaleZ={3}
-              rotY={90}
-              x={15}
-            // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
-            // rotY={step.rotation[1]}
-            /> */}
+            <Sides rotY={90} x={-15} scaleX={3} scaleY={random.range(2, 4)} />
+
+
+
+            <Sides rotY={90} x={15} scaleX={3} scaleY={random.range(2, 4)} />
+
+
+
+
 
             <GroundGroup />
             {/* <PlantsDecoration /> */}
@@ -206,7 +200,7 @@ const ValleyWalls = () => {
 
 
 
-            {step.index % 3 === 0  && (
+            {step.index % 3 === 0 && (
               <>
                 <Prefab
                   id="gpl_booster_plate_02"
@@ -221,20 +215,7 @@ const ValleyWalls = () => {
 
             )}
 
-            {/* {step.index % 6 === 1 && (
-              <>
-                <Damaging amount={50} knockbackStrength={200} >
-                <Prefab
-                  id="waffle_01"
-                  material="t_lava_01"
-                  x={random.range(-10, 10)}
-                  y={0}
-                  rotY={random.range(0, 360)}
-                  scale={1}
-                />
-                </Damaging>
-              </>
-            )} */}
+
 
 
             {step.index % 10 === 1 && step.index > 20 && (
@@ -243,29 +224,57 @@ const ValleyWalls = () => {
 
             )}
 
-            {step.index % 100 === 1 && step.index > 20 && (
+            {step.index % 10 === 1 && step.index > 20 && (
 
-              <Prefab 
-              id="collectible_mandatory_key_01" 
-              material='t_rainbow_02'
-              y={-5}
-              rotY={90} 
-              scale={7} 
+              <Prefab
+                id="flag_01"
+                material='t_dirt_01'
+                y={0}
+                // rotY={90}
+                scale={5}
               />
 
             )}
 
+            {step.index % 15 == 0 && step.index > 10 && (
+              <>
+                <Animation animation={{ y: [0, 10], x: [0, 0], rotY: [0, 0], duration: 2 }}>
+                  <Prefab
+                    id="rotten_fence_01"
+                    // material='t_rainbow_02'
+                    y={0}
+                    // rotY={90} 
+                    x={-8}
+                    scale={4}
+                    
+                  />
+                </Animation>
 
-                        
-            {step.index % 50 === 1 && step.index > 20  && (
+                <Animation animation={{ y: [10, 0], x: [0, 0], rotY: [0, 0], duration: 2 }}>
+                  <Prefab
+                    id="rotten_fence_01"
+                    // material='t_rainbow_02'
+                    y={0}
+                    // rotY={90} 
+                    x={8}
+                    scale={4}
+                  />
+                </Animation>
+              </>
 
-            <Prefab 
-            id="checkpoint_01" 
-            // material='t_rainbow_02'
-            y={0}
-            rotY={90} 
-            scale={5} 
-            />
+            )}
+
+
+
+            {step.index % 50 === 1 && step.index > 20 && (
+
+              <Prefab
+                id="checkpoint_01"
+                // material='t_rainbow_02'
+                y={0}
+                rotY={90}
+                scale={5}
+              />
 
             )}
 
@@ -285,79 +294,24 @@ const Goal = () => (
   <>
     <Prefab
       id="goal_01"
-      material="t_planks_02"
-      scaleY={3}
-      x={624.9} y={0.0} z={179.5}
+      material="t_rainbow_02"
+      scale={3}
+      x={632.9} y={0} z={191.4}
     />
-    <Prefab
-      id="rock_cube_01_t2"
-      material="t_planks_02"
-      scaleY={1}
-      x={291.4} y={-2.0} z={125.8}
-      scaleX={5}
-      scaleZ={5}
-    />
+
   </>
 )
 
-const Treehouse = () => (
+const GoalDecoration = () => (
   <Prefab
-    id="tree_house_03"
+    id="tree_house_04"
     material="t_wood_tile_02"
     scaleY={1}
-    x={5}
-    y={0}
-    z={8}
-    scale={4}
-    rotY={-90}
+    x={611.3} y={0.0} z={174.1}
+    scale={1}
+    rotY={120}
   />
 )
-
-const MossyMound = () => (
-  <Path
-    showPoints={false}
-    numberOfItems={20}
-    points={[
-      [623, 0, 178],
-      [623, 0, 178]
-      
-      // [400, 0, 300],
-    ]}
-    // close={true}
-    renderItem={step => {
-      // console.log(step.rotation[1])
-      return (
-        <HNode>
-
-
-          {step.index % 2 === 0 && (
-            <Prefab
-              id="cliff_01_wall"
-              // material="t_rock_02"
-              scaleY={1}
-            // z={-10}
-            // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
-            // rotY={step.rotation[1]}
-            />
-          )}
-
-          <Prefab
-            id="h_vines_01"
-            // material="t_rock_02"
-            scaleY={2}
-            y={5}
-          // z={1}
-
-          // scale={0.5 + Math.sin(step.progress * Math.PI) * 1}
-          // rotY={step.rotation[1]}
-          />
-
-
-        </HNode>
-      )
-    }}></Path>
-)
-
 
 
 
@@ -380,14 +334,14 @@ const World = () => (
     <ValleyWalls />
     <Goal />
     {/* <Treehouse /> */}
-    {/* <MossyMound /> */}
+    <GoalDecoration />
     <WelcomeSign x={74.4} y={0.0} z={57.5} rotY={-90} />
 
 
   </HNode >
 );
 
-render(<World />, { environment: 'hiberpunk_smog_01' });
+render(<World />, { environment: 'midday_01' });
 
 // questions
 // two prefabs in one step
