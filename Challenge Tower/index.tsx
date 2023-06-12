8beab60a663517f5552b2dc504016eddfd67968e
lucashadin <62254402+lucashadin@users.noreply.github.com>
2023-06-12 16:15:55 +0200

    tweaks

--- Challenge Tower/index.tsx
+++ Challenge Tower/index.tsx
@@ -292,7 +292,7 @@ const BoopingWalls: HDKComponent = ({ ...props }) => {
           y={1}
           // rotY={90} 
           x={-10}
-          scaleX={2}
+          scaleX={2.5}
           scaleZ={3}
           scaleY={3}
           rotY={90}
@@ -480,7 +480,7 @@ const Hole: HDKComponent = props => (
 
 
 const ThumbnailAll: HDKComponent = props => (
-<HNode x={10} z={-6}>
+<HNode x={-10} z={-6} y={-2}>
 <ImagePanel src="https://images.unsplash.com/photo-1530667912788-f976e8ee0bd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80" 
 ratio={1400 / 750} z={0} y={6} x={0} rotY={90} scale={3} />
 <ImagePanel src="https://images.unsplash.com/photo-1579202601184-34a979c427bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80" 
@@ -649,7 +649,7 @@ const FlyingDeath: HDKComponent<{ durationMin: number; durationMax: number, dire
   const random = useRandom();
   const duration = random.range(durationMin, durationMin);
   const startAt = random.range(0, 20);
-  var zRange = random.range(60, 70);
+  var zRange = random.range(60, 80);
 
   const { boopMultiplier, colourLight, colourDark, flyingDamage } = useLevelConfig();
 
--- Demos/index.tsx
+++ Demos/index.tsx
@@ -11,28 +11,34 @@ import {
   Spawnpoint,
   VideoPanel,
 } from '@hiber3d/hdk-react-components';
- 
+
 const Foliage: HDKComponent = props => {
   return (
-    <Distribute
-      {...props}
-      gladeRadius={20}
-      renderItem={item => {
-        const random = useRandom();
- 
-        return (
-          <RandomTilt scale={random.range(1, 2)}>
-            <Prefab id={random.fromArray(['tree_02', 'dead_tree_02', 'mushroom_01_t3'])} />
-          </RandomTilt>
-        );
-      }}
-    />
+    <>
+      <Prefab id="rock_pile_01_t1" rotY={210} x={4} y={0} scale={1} />
+      <Distribute
+        {...props}
+        gladeRadius={20}
+
+        outerBoundRadius={100}
+        renderItem={item => {
+          const random = useRandom();
+
+          return (
+            <RandomTilt scale={random.range(1, 3)}>
+              <Prefab id={random.fromArray(['rock_pile_01_t1', 'bush_01', 'tree_02'])} />
+            </RandomTilt>
+          );
+        }}
+      />
+    </>
   );
 };
- 
+
 const OrbitingAsteroids: HDKComponent = props => {
   return (
     <HNode {...props}>
+      <Prefab id="rock_01_t1" rotY={210} x={4} y={0.1} scale={2} />
       <Orbiting duration={64}>
         <Distribute
           gladeRadius={40}
@@ -41,10 +47,10 @@ const OrbitingAsteroids: HDKComponent = props => {
           itemAreaSizeMax={25}
           renderItem={item => {
             const random = useRandom();
- 
+
             return (
               <AsteroidSpinning y={random.range(-10, 10)} scale={random.range(1, 5)}>
-                <Prefab id={random.fromArray(['house_01'])} />
+                <Prefab id={random.fromArray(['rock_01_t1'])} />
               </AsteroidSpinning>
             );
           }}
@@ -53,7 +59,7 @@ const OrbitingAsteroids: HDKComponent = props => {
     </HNode>
   );
 };
- 
+
 const Building: HDKComponent = props => {
   return (
     <Room
@@ -67,47 +73,31 @@ const Building: HDKComponent = props => {
       // wallMaterial={'t_neon_grid_01'}
       wallTypes={['WINDOW', 'WINDOW', 'DOOR', 'WINDOW']}>
       <Prefab id="sofa_01_t2" rotY={180} scale={2}>
-        <Avatar animation="an_default_backflip" y={0.16} z={0.12} rotY={20} />
+        <Avatar animation="an_default_emote_applaud" scale={0.75} y={0.7} z={0.12} rotY={20}
+          src={{
+            url: 'https://models.readyplayer.me/631cdcd13a656b9c32f6ef7e.glb',
+            skeletonGroupID: 'skg_rpm_male',
+          }} />
       </Prefab>
       <Prefab id="floor_lamp_01" rotY={210} x={4} y={0.1} scale={2} />
       <VideoPanel src={'https://cdn.hibervr.com/video/Hiber3D.mp4'} y={7} z={-8.5} scale={5} rotY={180}></VideoPanel>
     </Room>
   );
 };
- 
-
-const PythonPath: HDKComponent = props => (
-  <Path
-  {...props}
-    points={[
-      [57, 0, 45], [57, 0, 96], [66, 0, 156], [120, 0, 195], [177, 0, 216], [234, 0, 189], [285, 0, 132], [261, 0, 45], [216, 0, 33], [180, 0, 54], [150, 0, 108], [129, 0, 156], [102, 0, 231], [96, 0, 282], [105, 0, 357], [120, 0, 405], [153, 0, 432], [198, 0, 453], [261, 0, 462], [312, 0, 462], [375, 0, 435], [411, 0, 342], [393, 0, 282], [339, 0, 273], [300, 0, 297], [261, 0, 360], [240, 0, 411], [225, 0, 474], [222, 0, 531], [222, 0, 579], [243, 0, 606], [270, 0, 639], [315, 0, 666], [366, 0, 684], [417, 0, 678], [474, 0, 651], [531, 0, 609], [573, 0, 537]
 
 
-    ]}
-    numberOfItems={100}
-    tension={0.8}
-    showPoints={true}
-    renderItem={() => (
-      <>
-        <Prefab id="cube_01" scaleZ={10}/>
-        {/* <Prefab id="gpl_booster_plate_01" y={3} rotY={-180} /> */}
-      </>
-    )}
-  />
-  )
-  
+
 
 const World = () => (
   <HNode>
-    <Foliage y={-1.5} />
-    <OrbitingAsteroids y={20} />
-    <Building y={0} z={20} />
+    {/* <Foliage y={-1.5} /> */}
+    {/* <OrbitingAsteroids y={20} /> */}
+    {/* <Building y={2} z={20} /> */}
     <Spawnpoint />
-    {/* <PythonPath y={1}/> */}
-    <Ground hilly={3} material="t_grass_03" />
-    <Ground hilly={3} material="t_swamp_ground" water={true} y={-1}/>
-    <Ground hilly={20} y={45} rotZ={180} material="t_cobble_stone_01" />
+    <Ground hilly={0} material="t_grass_01" />
+    {/* <Ground hilly={-1} material="t_swamp_ground" water={true} y={-0.5} /> */}
+    {/* <Ground hilly={30} y={45} rotZ={180} material="t_cobble_stone_01" /> */}
   </HNode>
 );
- 
+
 render(<World />, { environment: 'midday_clear_01' });
